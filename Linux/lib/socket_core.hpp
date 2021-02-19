05989179cbe38a56de251b858392c066a89d34b8
I-Rinka <I_Rinka@outlook.com>
2021-02-19 18:52:01 +0800

    unlimited socket length at on send

--- Linux/lib/socket_core.hpp
+++ Linux/lib/socket_core.hpp
@@ -42,24 +42,16 @@ private:
             });
     }
 
-    void do_read_header()
+    void do_read_len()
     {
-        boost::asio::async_read(*this->socket_, boost::asio::buffer(this->msg_len, 10),
+        boost::asio::async_read(*this->socket_, boost::asio::buffer(this->rcv_len, 10),
                                 [this](boost::system::error_code ec, std::size_t /*length*/) {
                                     if (!ec)
                                     {
-                                        int len = atoi(this->msg_len);
-                                        if (len > message_length)
-                                        {
-                                            if (message != NULL)
-                                            {
-                                                delete this->message;
-                                            }
-                                            this->message = (char *)malloc(len);
-                                        }
-                                        this->message_length = len;
+                                        int len = atoi(this->rcv_len);
+                                        this->message_remain_to_read = len;
 
-                                        this->do_read_boady();
+                                        this->do_read();
                                     }
                                     else
                                     {
@@ -72,15 +64,27 @@ private:
                                     }
                                 });
     }
-    void do_read_boady()
+    void do_read()
     {
 
-        boost::asio::async_read(*this->socket_, boost::asio::buffer(this->message, this->message_length),
+        boost::asio::async_read(*this->socket_, boost::asio::buffer(this->message_read, this->message_remain_to_read),
                                 [this](boost::system::error_code ec, std::size_t /*length*/) {
+                                    this->message_remain_to_read -= 4096;
                                     if (!ec)
                                     {
-                                        std::cout << this->message << std::endl;
-                                        this->do_read_header();
+                                        if (this->message_remain_to_read > 0)
+                                        {
+                                            std::cout << this->message_remain_to_read << std::endl;
+                                            this->message_remain_to_read += 4096;
+                                            this->do_read();
+                                        }
+                                        else
+                                        {
+                                            this->message_read = this->message_read_head;
+                                            memset(this->message_read, 0, INT32_MAX);
+                                            this->message_remain_to_read = 0;
+                                            this->do_read_len();
+                                        }
                                     }
                                     else
                                     {
@@ -93,17 +97,17 @@ private:
                                     }
                                 });
     }
-
-    void do_write()
+    void do_write_len()
     {
-        sprintf(this->msg_len, "%10d", this->message_length);
+        char msg_len[11];
+        sprintf(msg_len, "%10d", this->message_remain_to_write);
         // boost::asio::write(*socket_, boost::asio::buffer(this->msg_len, 11));
         boost::asio::async_write(*socket_,
-                                 boost::asio::buffer(this->msg_len, 10),
+                                 boost::asio::buffer(msg_len, 10),
                                  [this](boost::system::error_code ec, std::size_t /*length*/) {
                                      if (!ec)
                                      {
-                                         //   do_write();
+                                         do_write();
                                      }
 
                                      else
@@ -111,12 +115,26 @@ private:
                                          std::cout << ec.value() << std::endl;
                                      }
                                  });
+    }
+    void do_write()
+    {
         boost::asio::async_write(*socket_,
-                                 boost::asio::buffer(this->message, this->message_length),
+                                 boost::asio::buffer(this->message_write, this->message_remain_to_write),
                                  [this](boost::system::error_code ec, std::size_t /*length*/) {
                                      if (!ec)
                                      {
-                                         //   do_write();
+                                         this->message_remain_to_write -= 4096;
+                                         if (this->message_remain_to_write > 0)
+                                         {
+                                             this->message_remain_to_write += 4096;
+                                             do_write(); //继续写
+                                         }
+                                         else
+                                         {
+                                             this->message_remain_to_write = 0;
+                                             this->message_write = this->message_write_head;
+                                             memset(this->message_write, 0, INT32_MAX);
+                                         }
                                      }
 
                                      else
@@ -130,6 +148,9 @@ private:
     {
         do_read_header();
     }
+    char *message_write_head = NULL;
+    char *message_read_head = NULL;
+    char rcv_len[11];
 
 public:
     Socket_Core(const char *host, const char *port);
@@ -138,12 +159,17 @@ public:
     boost::asio::io_context *io_context_;
     tcp::socket *socket_;
     tcp::acceptor *acceptor_;
-    char *message = NULL;
-    char msg_len[11];
-    int message_length = 0;
+
+    char *message_write = NULL;
+    char *message_read = NULL;
+    int message_remain_to_write = 0;
+    int message_remain_to_read = 0;
+
     void write(char *msg)
     {
         int len = strnlen(msg, INTMAX_MAX);
+        this->message_remain_to_write = len;
+        /*
         if (len > message_length)
         {
             if (message != NULL)
@@ -153,26 +179,36 @@ public:
             this->message = (char *)malloc(len);
         }
         this->message_length = len;
-        strncpy(this->message, msg, message_length);
-        do_write();
+        */
+        strncpy(this->message_remain_to_write, msg, len);
+        do_write_len();
     }
 };
 
 Socket_Core::Socket_Core(const char *host, const char *port)
 {
     this->io_context_ = new boost::asio::io_context();
+    this->message_read = (char *)malloc(INT32_MAX);
+    this->message_write = (char *)malloc(INT32_MAX);
+    this->message_write_head = this->message_write;
+    this->message_read_head = this->message_read;
 
     tcp::resolver resolver = tcp::resolver(*this->io_context_);
 
     this->socket_ = new tcp::socket(*this->io_context_);
 
     boost::asio::connect(*this->socket_, resolver.resolve(host, port));
+
     do_regist();
     std::thread *t = new std::thread([this]() { this->io_context_->run(); });
 }
 Socket_Core::Socket_Core(int port)
 {
     this->io_context_ = new boost::asio::io_context();
+    this->message_read = (char *)malloc(INT32_MAX);
+    this->message_write = (char *)malloc(INT32_MAX);
+    this->message_write_head = this->message_write;
+    this->message_read_head = this->message_read;
 
     this->acceptor_ = new tcp::acceptor(*this->io_context_, tcp::endpoint(tcp::v4(), port));
     first_accept();
