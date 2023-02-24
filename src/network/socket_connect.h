55407d97efd813591239fa5d47f592fec816c54d
I-Rinka <I_Rinka@outlook.com>
2023-02-24 15:32:29 +0000

    updated server and client implementation

--- src/network/socket_connect.h
+++ src/network/socket_connect.h
@@ -17,85 +17,58 @@ protected:
     const static size_t DEFAULT_BUFFER_SIZE = 1024;
 
 public:
+    std::string IP;
+    uint16_t port = 0;
+
+    TCPSocket();
+
     // Ownership stuff
+    TCPSocket(TCPSocket &&other);
+    TCPSocket(int &&sockfd);
     TCPSocket(const TCPSocket &) = delete;
     TCPSocket &operator=(const TCPSocket &) = delete;
-    TCPSocket(TCPSocket &&other)
-    {
-        _sockfd = other._sockfd;
-        other._sockfd = -1;
-
-        _buffer = other._buffer;
-        _buffer_size = other._buffer_size;
-        other._buffer_size = 0;
-        other._buffer = nullptr;
-    };
-
-    TCPSocket()
-    {
-        _sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (_sockfd == -1)
-        {
-            perror("socket");
-            throw std::runtime_error("Error creating socket");
-        }
-
-        _buffer = new char[DEFAULT_BUFFER_SIZE];
-        _buffer_size = DEFAULT_BUFFER_SIZE;
-    };
 
     //! \brief Socket send data in the string
     //! \return The number of byte that is sent. Which means it might not send all of the chars in the string
-    virtual size_t send(const std::string &data) const
-    {
-        if (_sockfd == -1)
-        {
-            throw std::runtime_error("Socket is not created before send");
-        }
-
-        int sent = ::send(_sockfd, data.c_str(), data.size(), 0);
-        if (sent == -1)
-        {
-            throw std::runtime_error("Packet sent error");
-        }
-        return sent;
-    }
+    virtual size_t send(const std::string &data);
 
     //! \brief Socket receive data stored in the string
     //! \return std::string that stores data
-    virtual std::string recv() const
-    {
-        if (_sockfd == -1)
-        {
-            throw std::runtime_error("Socket is not created before receive");
-        }
+    virtual std::string recv();
 
-        int recved = ::recv(_sockfd, _buffer, _buffer_size - 1, 0);
-        if (recved == -1)
-        {
-            throw std::runtime_error("Packet sent error");
-        }
-        _buffer[recved] = 0;
-
-        return std::string(_buffer);
-    }
-
-    // Ownership stuff
-    TCPSocket(int &&sockfd)
-    {
-        _sockfd = sockfd;
-    }
-
-    virtual ~TCPSocket()
-    {
-        close(_sockfd);
-    };
+    virtual ~TCPSocket();
 
     //! \brief Make this class work with system api
-    operator int() const
-    {
-        return this->_sockfd;
-    }
+    operator int() const;
+};
+
+class ClientSocket
+{
+protected:
+    TCPSocket *_socket;
+
+public:
+    ClientSocket(const ClientSocket &) = delete;
+    ClientSocket &operator=(const ClientSocket &) = delete;
+
+    //! \brief Claim client socket without address
+    //! \note If you call the default version, you need to call .connect after that
+    ClientSocket() : _socket(new TCPSocket()){};
+    ClientSocket(std::string ip, uint16_t port) : _socket(new TCPSocket()) { connect(ip, port); };
+
+    //! \brief Connect to the target server
+    bool connect(std::string ip, uint16_t port);
+
+    //! \brief Socket send data in the string
+    //! \return The number of byte that is sent. Which means it might not send all of the chars in the string
+    virtual size_t send(const std::string &data) { return _socket->send(data); };
+
+    //! \brief Socket receive data stored in the string
+    //! \return std::string that stores data
+    virtual std::string recv() { return _socket->recv(); };
+
+    //! \brief This is close() semetics of socket
+    virtual ~ClientSocket() = default;
 };
 
 class ServerSocket
@@ -104,6 +77,9 @@ protected:
     TCPSocket *_socket;
 
 public:
+    ServerSocket(const ServerSocket &) = delete;
+    ServerSocket &operator=(const ServerSocket &) = delete;
+
     //! \brief Claim server socket without bind address
     //! \note If you call the default version, you need to call .bind(port) after that
     ServerSocket() : _socket(new TCPSocket()){};
@@ -126,7 +102,103 @@ public:
 
     //! \brief This is close() semetics of socket
     virtual ~ServerSocket() = default;
+
+    //! \brief Socket send data in the string
+    //! \return The number of byte that is sent. Which means it might not send all of the chars in the string
+    virtual size_t send(const std::string &data) { return _socket->send(data); };
+
+    //! \brief Socket receive data stored in the string
+    //! \return std::string that stores data
+    virtual std::string recv() { return _socket->recv(); };
 };
+
+// ---------------------------------------Implementation----------------------------------------
+
+/**
+ * @brief TCPSocket Class Implementation
+ */
+
+TCPSocket::TCPSocket()
+{
+    _sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (_sockfd == -1)
+    {
+        throw std::runtime_error("Error creating socket");
+    }
+
+    _buffer = new char[DEFAULT_BUFFER_SIZE];
+    _buffer_size = DEFAULT_BUFFER_SIZE;
+};
+
+TCPSocket::TCPSocket(TCPSocket &&other)
+{
+    _sockfd = other._sockfd;
+    other._sockfd = -1;
+
+    _buffer = other._buffer;
+    _buffer_size = other._buffer_size;
+    other._buffer_size = 0;
+    other._buffer = nullptr;
+};
+
+// Ownership stuff
+TCPSocket::TCPSocket(int &&sockfd)
+{
+    _sockfd = sockfd;
+
+    _buffer = new char[DEFAULT_BUFFER_SIZE];
+    _buffer_size = DEFAULT_BUFFER_SIZE;
+}
+
+TCPSocket::~TCPSocket()
+{
+    close(_sockfd);
+    if (_buffer != nullptr)
+    {
+        delete _buffer;
+    }
+};
+
+size_t TCPSocket::send(const std::string &data)
+{
+    if (_sockfd == -1)
+    {
+        throw std::runtime_error("Socket is not created before send");
+    }
+
+    int sent = ::send(_sockfd, data.c_str(), data.size(), 0);
+    if (sent == -1)
+    {
+        throw std::runtime_error("Packet sent error");
+    }
+    return sent;
+}
+
+std::string TCPSocket::recv()
+{
+    if (_sockfd == -1)
+    {
+        throw std::runtime_error("Socket is not created before receive");
+    }
+
+    int recved = ::recv(_sockfd, _buffer, _buffer_size - 1, 0);
+    if (recved == -1)
+    {
+        throw std::runtime_error("Packet sent error");
+    }
+    _buffer[recved] = 0;
+
+    return std::string(_buffer);
+}
+
+TCPSocket::operator int() const
+{
+    return this->_sockfd;
+}
+
+/**
+ * @brief ServerSocket Class Implementation
+ */
 
 ServerSocket::ServerSocket(uint16_t port) : _socket(new TCPSocket())
 {
@@ -142,6 +214,9 @@ bool ServerSocket::bind(uint16_t port)
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
+    _socket->IP = "0.0.0.0";
+    _socket->port = port;
+
     int ret = ::bind(*_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
     return ret == 0;
 }
@@ -154,7 +229,36 @@ bool ServerSocket::listen(int connection_volume = 100)
 TCPSocket ServerSocket::accept()
 {
     int socklen = sizeof(sockaddr_in);
-    struct sockaddr_in clientaddr;
+    struct sockaddr_in client_addr;
 
-    return ::accept(*_socket, (struct sockaddr *)&clientaddr, (socklen_t *)&socklen);
+    TCPSocket sock = ::accept(*_socket, (struct sockaddr *)&client_addr, (socklen_t *)&socklen);
+
+    sock.port = client_addr.sin_port;
+
+    char client_ip[INET_ADDRSTRLEN];
+
+    // Get ip string from client
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+    sock.IP = client_ip;
+
+    return sock;
+}
+
+/**
+ * @brief Client Socket Class Implementation
+ */
+
+bool ClientSocket::connect(std::string ip, uint16_t port)
+{
+    _socket->IP = ip;
+    _socket->port = port;
+
+    sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+
+    int ret = ::connect(*_socket, (sockaddr *)(&server_addr), sizeof(server_addr));
+    return ret != -1;
 }
