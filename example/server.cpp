7b63fc689429b34047a2f5810f2622b5ccf66592
I-Rinka <I_Rinka@outlook.com>
2023-03-03 15:36:51 +0000

    Server OK

--- example/server.cpp
+++ example/server.cpp
@@ -26,10 +26,15 @@ std::tuple<int, int> add_count(int add)
 
 int main(int argc, char const *argv[])
 {
-    ss.bind(RPCs::add_count, add_count);
-    ss.bind(RPCs::divide, divide);
-    ss.bind(RPCs::print_string, print_string);
-    
+    ss.bind(RPCs::add_count, [](int add)
+            { return add_count(add); });
+    ss.bind(RPCs::divide, [](double a, double b)
+            { return divide(a, b); });
+
+    // ss.bind(RPCs::print_string, &print_string);
+    ss.bind(RPCs::print_string, [](string str)
+            { print_string(str); });
+
     ss.start();
     return 0;
 }
--- src/server/function_call_tule.h
+++ src/server/function_call_tule.h
@@ -17,10 +17,12 @@ struct __call_helper<0>
     template <typename Function, typename... Args>
     static auto call(Function F, typename function_traits<Function>::args_tuple, Args... args) -> typename function_traits<Function>::return_type
     {
+        // you can only pass functor / lambda to this function
         return F(args...);
     }
 };
 
+//! \brief you can only pass functor / lambda to this function
 template <typename Function>
 auto CallTuple(Function F, typename function_traits<Function>::args_tuple params_tp) -> typename function_traits<Function>::return_type
 {
