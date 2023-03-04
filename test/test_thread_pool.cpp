24a36a71bbd560a2846ba2ff93d45e1bde0f9760
I-Rinka <I_Rinka@outlook.com>
2023-03-04 12:42:52 +0000

    There must be some deadlock

--- test/test_thread_pool.cpp
+++ test/test_thread_pool.cpp
@@ -6,11 +6,17 @@ int main(int argc, char const *argv[])
     ThreadPool tp(10);
 
     tp.AddTask([]
-               { cout << "Hello world" << endl; this_thread::sleep_for(chrono::milliseconds(100)); });
+               { cout << "Hello world" << endl; 
+            //    this_thread::sleep_for(chrono::milliseconds(100)); 
+               });
 
     // future.get();
 
-    tp.~ThreadPool();
     cout << "你好" << endl;
+    cout << "你好2" << endl;
+    cout << "你好3" << endl;
+    cout << "你好4" << endl;
+    // this_thread::sleep_for(chrono::milliseconds(100));
+    cout << "running threads:" << tp.GetWorkingThreadsNum() << endl;
     return 0;
 }
